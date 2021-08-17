1bd9e7c09c7cc790e5d518beb9e01a407c6aa565
movistar18 <89030054+movistar18@users.noreply.github.com>
2021-08-17 10:05:37 -0500

    Create Error discord.js

---
+++ Error discord.js
@@ -0,0 +1,4 @@
+Posiblemente tengas un error al momento de crear el bot, posiblemente sea porque ahora discord.js tiene una nueva versión
+En este caso, tendremos que cambiar la versión, para seguir usando la que teniamos, para eso pueden ir al siguiente video:
+//https://www.youtube.com/watch?v=6dHIGEp3R3s//
+Ahi tendran la solución, ya saben que si aun tienen mas errores pueden escribirme al discord, me encuentran como "movsitarm#1268"
